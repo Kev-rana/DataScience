f96e9f29fe8554d52454f1d45295293ec89fca6a
Kev-rana <kevalrn@gmail.com>
2022-04-05 20:29:08 +0530

    Add files via upload

---
+++ numpy1.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 16 13:50:00 2019
+
+@author: Kev
+"""
+import numpy as np
+baseball = [180,205 ,210,120,209,186,200]
+np_baseball=np.array(baseball)
+print (np_baseball)
+print(type(np_baseball))
